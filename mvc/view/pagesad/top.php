da455ae0e6439fcc293b7ffc98cd4881204df695
tnhao962 <126378816+tnhao962@users.noreply.github.com>
2024-03-27 21:18:12 +0700

    Delete mvc directory

--- mvc/view/pagesad/top.php
+++
@@ -1,8 +0,0 @@
-<div class="topbar">
-                <div class="toggle">
-                <ion-icon name="menu-outline"></ion-icon>
-                </div>
-                <div class="user">
-                    <input id="js-buton"  type="image" src="public/img/avt.jpg" alt="">
-                </div>
-            </div>
